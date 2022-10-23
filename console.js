e0115ebdeb22f2b6d124eaac40adbd2a034b1b1b
BerserSK <andres2003latorre@gmail.com>
2022-10-23 18:19:15 -0500

    Calendario Octubre terminado

--- console.js
+++ console.js
@@ -364,3 +364,181 @@ function AQ(){
 }
 
 
+// Calendario ------------------------
+function schedule(){
+  rl.question('---> Ingresa el mes que desea visualizar: ', function (optC) {
+    console.log(`---> Ingresaste la opción: ${optC}`);
+    console.log('                                            ');
+    calendario(optC);
+  
+   // rl.close();
+  });
+  
+  function calendario(optC){
+    console.log("============================================");
+    console.log("|            FECHA Y HORA ACUAL            |");
+    console.log("|                    +                     |");
+    console.log("|                CALENDARIO                |");
+    console.log("============================================");
+    console.log("|                                          |");
+    const fecha = new Date();
+    const hoy = fecha.getDate();
+    const mes = fecha.getMonth();
+    const año = fecha.getFullYear();
+    const hora = fecha.getHours();
+    const minutos = fecha.getMinutes();
+    console.log("|=== hoy es",hoy, "del mes",mes, "del año",año, "=====|");
+    console.log("|==========" ,"la hora es",hora,":",minutos ,"============|");
+    console.log("                                                      ");
+    switch(optC){
+      case "Octubre":
+        console.log("==================================================");
+        console.log("|                   CALENDARIO                   |");
+        console.log("==================================================");
+        console.log("| DOM |  LUN |  MAR | MIER  | JUE  | VIE  |  SAB |");
+        console.log("|     |      |      |       |      |      |   1  |");
+        console.log("|------------------------------------------------|");
+        console.log("|  2   |  3  |  4   |   5   |  6    |  7   |  8  |");
+        console.log("|------------------------------------------------|");
+        console.log("|  9   |  10 |  11  |   12  |   13  |  14  | 15  |");
+        console.log("|------------------------------------------------|");
+        console.log("|  16  |  17 |  18  |   19  |   20  |  21  | 22  |");
+        console.log("|------------------------------------------------|");
+        console.log("|  23  |  24 |  25  |   26  |   27  |  28  | 29  |");
+        console.log("|------------------------------------------------|");
+        console.log("|  30  |  31 |      |       |       |      |     |");
+        console.log("|================================================|");
+        console.log("|                                                |");
+        rl.question('---> Ingrese el dia desde el que quiere calcular: ',function(dia){
+          switch(dia){
+            case "1":
+              console.log("                                          ");
+              console.log(`Faltan 30 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "2":
+              console.log("                                          ");
+              console.log(`Faltan 29 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "3":
+              console.log("                                          ");
+              console.log(`Faltan 28 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "4":
+              console.log("                                          ");
+              console.log(`Faltan 27 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "5":
+              console.log("                                          ");
+              console.log(`Faltan 26 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "6":
+              console.log("                                          ");
+              console.log(`Faltan 25 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "7":
+              console.log("                                          ");
+              console.log(`Faltan 24 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "8":
+              console.log("                                          ");
+              console.log(`Faltan 23 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "9":
+              console.log("                                          ");
+              console.log(`Faltan 22 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "10":
+              console.log("                                          ");
+              console.log(`Faltan 21 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "11":
+              console.log("                                          ");
+              console.log(`Faltan 20 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "12":
+              console.log("                                          ");
+              console.log(`Faltan 19 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "13":
+              console.log("                                          ");
+              console.log(`Faltan 18 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "14":
+              console.log("                                          ");
+              console.log(`Faltan 17 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "15":
+              console.log("                                          ");
+              console.log(`Faltan 16 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "16":
+              console.log("                                          ");
+              console.log(`Faltan 15 dias para que sea 31 de Octubre del 2022`)
+            break;
+             case "17":
+              console.log("                                          ");
+              console.log(`Faltan 14 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "18":
+              console.log("                                          ");
+              console.log(`Faltan 13 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "19":
+              console.log("                                          ");
+              console.log(`Faltan 12 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "20":
+              console.log("                                          ");
+              console.log(`Faltan 11 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "21":
+              console.log("                                          ");
+              console.log(`Faltan 10 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "22":
+              console.log("                                          ");
+              console.log(`Faltan 9 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "23":
+              console.log("                                          ");
+              console.log(`Faltan 8 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "24":
+              console.log("                                          ");
+              console.log(`Faltan 7 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "25":
+              console.log("                                          ");
+              console.log(`Faltan 6 dias para que sea 31 de Octubre del 2022`)
+            break;
+             case "26":
+              console.log("                                          ");
+              console.log(`Faltan 5 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "27":
+              console.log("                                          ");
+              console.log(`Faltan 4 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "28":
+              console.log("                                          ");
+              console.log(`Faltan 3 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "29":
+              console.log("                                          ");
+              console.log(`Faltan 2 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "30":
+              console.log("                                          ");
+              console.log(`Faltan 1 dias para que sea 31 de Octubre del 2022`)
+            break;
+            case "31":
+              console.log("                                          ");
+              console.log(`Estamos  en 31 de Octubre del 2022, Felicidades!!`)
+              rl.close();
+            break;
+          }
+        })
+      break;
+    }
+  }
+}
